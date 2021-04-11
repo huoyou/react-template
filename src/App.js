acad61ada0ca9989c0bba376c39392b914db0935
huoyou <huoyou1234@gmail.com>
2021-04-12 00:00:48 +0800

    feat: 新增 husky+lint-staged+commitlint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line no-use-before-define
+/* eslint-disable */
 import React, { Fragment } from 'react';
 import Login from './pages/login';
 import Home from './pages/home';
@@ -9,7 +9,7 @@ function App() {
     <Fragment>
       <HashRouter>
         <Switch>
-            <Route path="/home" component={Home} />
+          <Route path="/home" component={Home} />
           <Route path="/" component={Login} />
           <Route exact path="/" component={Login} />
           <Redirect to={'/login'} />
